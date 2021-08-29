6dc67d149deaf2d1bc07d0797360f288fbb71403
sameet <wpf_sam@yahoo.com>
2021-08-29 13:38:55 +0430

    Creating backend for application

--- app.py
+++ app.py
@@ -5,6 +5,38 @@ from flask import render_template
 
 app = Flask(__name__)
 
+Admin = {
+    "username": 'admin',
+    "password": 'admin'
+}
+
+Proxy = {
+    "https": 'http://127.0.0.1:42585/'
+}
+
+
+def Responce():
+    r = requests.get("https://api.coinbase.com/v2/prices/buy?currency=USD", proxies=Proxy)
+    return r.json()
+
+
 @app.route("/")
 def index():
+    """ this founction is home pages. """
     return render_template("home.html")
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.form.get("username")
+    password = request.form.get("password")
+    if not username:
+        return "Error! Pleas enter your username"
+    if not password:
+        return "Error! Pleas enter your password"
+    if not username and not password:
+        return "Error! Pleas enter your username and password"
+    if username == Admin["username"] and password == Admin["password"]:
+        return render_template("admin.html", Responce=Responce())
+    if username != Admin["username"] and password != Admin["password"]:
+        return "Oh man you not did it Pleas try again! enter your username or password"
